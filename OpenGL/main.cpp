bf7bd4df7a320f4718e389cbacebf1e60edb7cc9
vigo walker <vigopaul05@gmail.com>
2022-11-10 10:49:13 -0300

    OpenGL simple window added

---
+++ OpenGL/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "glad.h"
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "TestScreen", NULL, NULL);
+
+    if (!window) {
+        cout << "Window started with problems" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}

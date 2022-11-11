cde09fb4e29e35bbc25c152d0f63433c3239be9c
vigo <vigopaul05@gmail.com>
2022-11-11 13:10:05 -0300

    added a 3d cube

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "glad/include/glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
 
 using namespace std;
 
@@ -10,6 +11,8 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     GLFWwindow* window = glfwCreateWindow(800, 800, "TestScreen", NULL, NULL);
 
@@ -25,16 +28,18 @@ int main() {
 
     glViewport(0, 0, 800, 800);
 
-    glClearColor(0, 0, 1, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+        glClearColor(0, 0, 1, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+
+        
+        glClearColor(1, 0, 0, 0);
     }
 
     glfwDestroyWindow(window);
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
 
---
+++ test1/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <GL/glut.h>
+
+using namespace std;
+
+void initGL() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClearDepth(1.0f);                   // Set background depth to farthest
+   glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+   glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+   glShadeModel(GL_SMOOTH);   // Enable smooth shading
+   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+}
+
+void Display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glFlush();
+    glMatrixMode(GL_MODELVIEW);
+
+    glLoadIdentity();
+    glTranslatef(1.5f, 0, -7.0f);
+
+    glBegin(GL_QUADS);
+        glColor3f(0, 1, 0);
+        glVertex3f(1.0f, 1.0f, -1.0f);
+        glVertex3f(-1.0f, 1.0f, -1.0f);
+        glVertex3f(-1.0f, 1.0f, 1.0f);
+        glVertex3f(1.0f, 1.0f, 1.0f);
+
+        glColor3f(1, 0.5f, 0);
+        glVertex3f( 1.0f, -1.0f,  1.0f);
+        glVertex3f(-1.0f, -1.0f,  1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f( 1.0f, -1.0f, -1.0f);
+
+        glColor3f(1, 0, 0);
+        glVertex3f( 1.0f,  1.0f, 1.0f);
+        glVertex3f(-1.0f,  1.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, 1.0f);
+        glVertex3f( 1.0f, -1.0f, 1.0f);
+
+        glColor3f(1, 1, 0);
+        glVertex3f( 1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f,  1.0f, -1.0f);
+        glVertex3f( 1.0f,  1.0f, -1.0f);
+
+        glColor3f(0, 0, 1);
+        glVertex3f(-1.0f,  1.0f,  1.0f);
+        glVertex3f(-1.0f,  1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f,  1.0f);
+
+        glColor3f(1, 0, 0);
+        glVertex3f(1.0f,  1.0f, -1.0f);
+        glVertex3f(1.0f,  1.0f,  1.0f);
+        glVertex3f(1.0f, -1.0f,  1.0f);
+        glVertex3f(1.0f, -1.0f, -1.0f);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
+   // Compute aspect ratio of the new window
+   if (height == 0) height = 1;                // To prevent divide by 0
+   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+ 
+   // Set the viewport to cover the new window
+   glViewport(0, 0, width, height);
+ 
+   // Set the aspect ratio of the clipping volume to match the viewport
+   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+   glLoadIdentity();             // Reset
+   // Enable perspective projection with fovy, aspect, zNear and zFar
+   gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+
+int main(int argc, char** argv) {
+    initGL();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(80, 80);
+    glutInitWindowSize(800, 800);
+
+    glutCreateWindow("Simple OpenGL");
+
+    glutDisplayFunc(Display);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+    return 0;
+}
